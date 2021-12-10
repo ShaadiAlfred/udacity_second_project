a4f05ab5297912b2857f01ac1c76e02bd468f32a
Shaadi Alfred <shaadi.alfred@gmail.com>
2021-12-11 00:35:28 +0200

    Create Product model

---
+++ src/database/models/Product.ts
@@ -0,0 +1,31 @@
+import { runQuery } from "..";
+
+export type Product = {
+  id?: number;
+  name: string;
+  // Price in cents
+  price: number;
+};
+
+export class ProductStore {
+  static async create(product: Product): Promise<Product> {
+    const result = await runQuery(
+      `INSERT INTO "products" (name, price)
+      VALUES ($1, $2)
+      RETURNING *`,
+      [product.name, String(product.price)],
+    );
+
+    return result.rows[0];
+  }
+
+  static async find(id: number): Promise<Product | null> {
+    const result = await runQuery(`SELECT * from "products" WHERE "id" = $1`, [String(id)]);
+
+    if (result.rowCount !== 0) {
+      return result.rows[0];
+    }
+
+    return null;
+  }
+}
