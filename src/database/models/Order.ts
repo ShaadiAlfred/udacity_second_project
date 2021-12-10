fe63db9dddd2245b11f0ab559bd66c0284aa5b73
Shaadi Alfred <shaadi.alfred@gmail.com>
2021-12-11 00:47:44 +0200

    Add Order model

---
+++ src/database/models/Order.ts
@@ -0,0 +1,31 @@
+import { runQuery } from "..";
+import { OrderStatus } from "../../types/OrderStatus";
+
+export type Order = {
+  id?: number;
+  userId: number;
+  status: OrderStatus;
+};
+
+export class OrderStore {
+  static async create(order: Order): Promise<Order> {
+    const result = await runQuery(
+      `INSERT INTO "orders" (userId, status)
+      VALUES ($1, $2)
+      RETURNING *`,
+      [order.userId, order.status],
+    );
+
+    return result.rows[0];
+  }
+
+  static async find(id: number): Promise<Order | null> {
+    const result = await runQuery(`SELECT * from "orders" WHERE "id" = $1`, [id]);
+
+    if (result.rowCount !== 0) {
+      return result.rows[0];
+    }
+
+    return null;
+  }
+}
