b1511853047b122ddc6583e1e66b04ea9e5dba92
Shaadi Alfred <shaadi.alfred@gmail.com>
2021-12-14 02:07:06 +0200

    Fix ordersProducts migration

--- src/database/migrations/sqls/20211213224511-create-orders-products-table-down.sql
+++ src/database/migrations/sqls/20211213224511-create-orders-products-table-down.sql
@@ -1,6 +1,1 @@
-CREATE TABLE ordersProducts (
-    id SERIAL PRIMARY KEY,
-    productId REFERENCES "products"(id),
-    quantity INTEGER,
-    orderId REFERENCES "orders"(id)
-);
+DROP TABLE ordersProducts;
--- src/database/migrations/sqls/20211213224511-create-orders-products-table-up.sql
+++ src/database/migrations/sqls/20211213224511-create-orders-products-table-up.sql
@@ -1,1 +1,6 @@
-DROP TABLE ordersProducts;
+CREATE TABLE ordersProducts (
+    id SERIAL PRIMARY KEY,
+    productId BIGINT REFERENCES "products"(id),
+    quantity INTEGER,
+    orderId BIGINT REFERENCES "orders"(id)
+);
